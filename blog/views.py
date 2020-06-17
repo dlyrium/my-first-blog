050cde0a19c8b9a66ab8a1c45d8997cbc2b337e4
dlyrium <crimsonthunder@gmail.com>
2020-06-17 02:03:35 +0100

    Added questions panel to polls. Failed to make it work in blog.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.views import generic
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
@@ -8,6 +9,7 @@ from .forms import PostForm, CommentForm
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('-published_date')
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
